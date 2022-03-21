531dc7608cc6a2cd60ea8d4d647a8e5a33190e22
connor-kilgore <cdk242@nau.edu>
2022-03-20 17:48:47 -0700

    added settings navigation

--- app/src/main/java/com/example/btrbudget/MainActivity.java
+++ app/src/main/java/com/example/btrbudget/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.btrbudget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get views of other layouts
+        View optionsScrn = getLayoutInflater().inflate(R.layout.options_screen, null);
+
+        ImageButton settingsBtn = (ImageButton) optionsScrn.findViewById(R.id.settingsBtn);
+        ImageButton homeBtn = (ImageButton) findViewById(R.id.homeBtn);
+    }
+
+    public void homeNavigate(View view)
+    {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void settingsNavigate(View view)
+    {
+        setContentView(R.layout.options_screen);
     }
 }
