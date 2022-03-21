954d26a153bebac938b0d0c1bcda59bbd65625e1
connor-kilgore <cdk242@nau.edu>
2022-03-20 22:05:40 -0700

    added Unit Tests

---
+++ app/src/test/java/com/example/btrbudget/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.btrbudget;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import android.view.View;
+
+import org.junit.jupiter.api.Test;
+
+class MainActivityTest {
+    @Test
+    void homeNavigate(View view){
+    }
+}
